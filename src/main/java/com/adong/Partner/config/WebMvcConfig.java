b289edabcb4765e3f700dfacbd215e9719b6edd7
沈仁东 <13803418+shen-rendong@user.noreply.gitee.com>
2024-06-13 13:14:42 +0800

    修改跨域配置

--- src/main/java/com/adong/Partner/config/WebMvcConfig.java
+++ src/main/java/com/adong/Partner/config/WebMvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * 跨域配置
  */
 @Configuration
-public class WebMvcConfg implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
